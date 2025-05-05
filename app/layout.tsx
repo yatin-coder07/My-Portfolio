dcd1082f8e06544abea32dc019e20b5e22de1179
yatin sharma <yatins113@gmail.com>
2025-05-05 22:06:09 +0530

    corrections

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
