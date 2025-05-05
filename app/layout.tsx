871cc1b8e0d2a58aa74015aa2ab176ba74715525
yatin sharma <yatins113@gmail.com>
2025-05-05 22:15:20 +0530

    config changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
